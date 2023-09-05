89dcada180878f688e632ce47a5cda4d0b7bdb2c
Oliver <112207456+owfinlay@users.noreply.github.com>
2023-09-05 04:25:32 -0400

    ishouldsleep

--- src/model.py
+++ src/model.py
@@ -3,18 +3,6 @@ import random
 import tflite_runtime.interpreter as tflite
 import numpy as np
 
-def _get_model():
-	model_path = "yamnet_classification.tflite"
-	interpreter = tflite.Interpreter(model_path)
-
-	input_details = interpreter.get_input_details()
-	print(input_details)
-
-	output_details = interpreter.get_output_details()
-	print("\n", output_details)
-
-	return interpreter
-
 def _normalize(arr: np.ndarray):
 	"""goto range [-1,1]; assumes all are positive"""
 	zerodiff = min(arr) + ((max(arr) - min(arr)) / 2)
@@ -22,20 +10,32 @@ def _normalize(arr: np.ndarray):
 	new_arr = new_arr / max(new_arr)
 	return new_arr
 
-def _classify(input_arr: np.ndarray, interpreter):
+def _classify(input_arr: np.ndarray):
 	"""Classification"""
-	nrmd_arr = _normalize(input_arr)
-	res = interpreter(nrmd_arr)
-	print(res)
-	return True
+	model_path = "yamnet_classification.tflite"
+	normed_arr = _normalize(input_arr)
 
+	interpreter = tflite.Interpreter(model_path)
+	input_index = interpreter.get_input_details()[0]['index']
+	output_index = interpreter.get_output_details()[0]['index']
 
+	interpreter.allocate_tensors()
+	interpreter.set_tensor(input_index, normed_arr)
+	interpreter.invoke()
 
-def _classify_fake(frames):
-	return random.random() >= 0.85
+	output_data = interpreter.get_tensor(output_index)
+	print(output_data)
+	return random.random >= 0.81
 
 def _test_response():
 	return "1"
 
+def _print_details():
+	model_path = "yamnet_classification.tflite"
+	interpreter = tflite.Interpreter(model_path)\
+
+	print("Input details: ", interpreter.get_input_details())
+	print("\nOutput details: ", interpreter.get_output_details())
+
 if __name__ == "__main__":
-	_get_model()
+	_print_details()
