6f4bd06039444f56ebc78c6a47cddee4bcf889f7
owfinlay <owfinlay@outlook.com>
2023-09-05 14:59:05 -0400

    machine considerations

---
+++ src/tflite_version.py
@@ -0,0 +1,18 @@
+import pathlib, os, platform
+
+path = pathlib.Path("resources/tflite_runtimes")
+
+base = "tflite_runtime-2.5.0.post1-cp39-cp39-linux_{}.whl"
+
+versions = {
+    "AARCH64": "aarch64",
+    "ARMV71": "armv71",
+    "x86_64": "x86_64"
+}
+machine = platform.machine()
+
+with open("requirements.txt", "a") as requirements:
+    requirements.write(
+        "\n",
+        base.format(versions[machine])
+    )
