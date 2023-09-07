df4be7e7a44bf8a6865a0ee1929331d93139c837
owfinlay <owfinlay@outlook.com>
2023-09-07 15:17:41 -0400

    edits for clarity/concision

--- src/model.py
+++ src/model.py
@@ -1,22 +1,18 @@
 """ml-related shenanigans"""
-import random
 import tflite_runtime.interpreter as tflite
 import numpy as np
-
-def get_model_path():
-	import os
-	return 
+import pathlib
 
 def normalize(arr: np.ndarray) -> np.ndarray:
-	"""goto range [-1,1]; assumes all are positive"""
+	"""linearly goto range [-1,1]; assumes all values are positive"""
 	zerodiff = (arr.max() + arr.min()) / 2
 	new_arr = (arr - zerodiff)
 	new_arr = (new_arr / new_arr.max()).flatten()
 	return new_arr[:15600]
 
-def _classify(input_arr: np.ndarray, sensitivity: int) -> bool:
-	"""Classification"""
-	model_path = "E:\\Learning\\DS_Comms\\upwork\\DDD\\src\\src\\yamnet_classification.tflite"
+def _classify(input_arr: np.ndarray, decision_threshold: int) -> bool:
+	"""model loading, setting & invocation"""
+	model_path = pathlib.Path("resources/yamnet_classification.tflite")
 	normed_arr = normalize(input_arr)
 
 	interpreter = tflite.Interpreter(model_path)
@@ -29,17 +25,14 @@ def _classify(input_arr: np.ndarray, sensitivity: int) -> bool:
 
 	output_data = interpreter.get_tensor(output_details[0]['index'])
 	bark_probability = output_data[0][70]
-	return(bark_probability >= (sensitivity * 0.1))
+	return(bark_probability >= (decision_threshold * 0.1))
 
-def _test_response():
-	return random.random() > 0.8
-
-def _print_details():
-	model_path = "yamnet_classification.tflite"
+def print_details():
+	model_path = pathlib.Path("resources/yamnet_classification.tflite")
 	interpreter = tflite.Interpreter(model_path)\
 
 	print("Input details: ", interpreter.get_input_details())
 	print("\nOutput details: ", interpreter.get_output_details())
 
 if __name__ == "__main__":
-	_print_details()
+	print_details()
